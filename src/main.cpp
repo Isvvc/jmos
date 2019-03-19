54e7c354f7bc747f36eaf549cc13b151a610c04a
Isvvc <lyons@tuta.io>
2019-03-18 18:16:03 -0600

    Separate sections of document into functions
    Separate sections of document into functions instead of having everything in one long main(). Also rework url shortening system.

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,20 @@
 using namespace std;
 using json = nlohmann::json;
 
+string game;
+
+enum URLs { mod, image };
+
+// Basically shorthand for getting commonly-used URLs
+string url(URLs type){
+	string output;
+	switch(type){
+		case mod:	return "https://www.nexusmods.com/" + game + "/mods/";
+		case image :	return "https://staticdelivery.nexusmods.com/mods/110/images/";
+	}
+	return "";
+}
+
 // Format a json string for printing
 string p(json input){
 	string text = input;
@@ -39,13 +53,112 @@ string linkify(std::string text) {
     return text;
 }
 
+// The below functions are for printing sections of the document
+
+//// Output a filtered list of all the mods from a given category
+stringstream filterCategory(json& mods, string& category, char columns){
+	stringstream output;
+
+	if(category!="null"){
+		stringstream line1, line2;
+		char col = 0;
+
+		output	<<"### Category: "<<category<<"\n\n";
+		line1	<<"|";
+		line2	<<"|";
+		for(char i = 0; i < columns; i++){
+			line1 <<"   |";
+			line2 <<"---|";
+		}
+		output	<< line1.rdbuf() << "\n" << line2.rdbuf() << "\n";
+
+		line1.str(string());
+		line2.str(string());
+		for(json::iterator it = mods.begin(); it != mods.end(); ++it){
+			if( jsonListContains(it.value()["categories"], category) ){
+				line1<<"| ["<<it.key()<<"](#"<<linkify(it.key())<<") ";
+				line2<<"| ![]("<< p(url(image)) << p(it.value()["main image"]) <<") ";
+				col++;
+				if(col > columns - 1){
+					col = 0;
+					output	<< line1.rdbuf() << "\n" << line2.rdbuf() << "\n";
+					line1.str(string());
+					line2.str(string());
+				}
+			}
+		}
+		if(col > 0){
+			output	<< line1.rdbuf() << "\n" << line2.rdbuf() << "\n";
+		}
+	}
+
+	return output;
+}
+
+//// Output a masterlist of all mods
+stringstream modMasterList(json& mods, json& categories, char columns){
+	stringstream output;
+
+	output <<"### Mod master list\n\n";
+	for(json::iterator it = mods.begin(); it != mods.end(); ++it){
+		stringstream line1, line2;
+
+		output	<<"\n#### "<<it.key()<<"\n\n"
+
+			<< p(it.value()["description"]) <<"\n\n"
+			<<"[Nexus link](" << p(url(mod)) << it.value()["id"] << ")\n\n";
+
+		line1	<<"| Images | ![]("<< p(url(image)) << p(it.value()["main image"]) <<") |";
+		line2	<<"| ------ |:---:|";
+		for(char i = 0; i < (columns - 2); i++){
+			line1 <<"   |";
+			line2 <<"---|";
+		}
+		output	<< line1.rdbuf() << "\n" << line2.rdbuf() << "\n";
+		{	char col = 0;
+			for(json::iterator jt = it.value()["images"].begin();
+					jt != it.value()["images"].end();
+					++jt, col = (col == columns - 1) ? 0 : col + 1 ){
+				output<<"| ![]("<< p(url(image)) << p(jt.value()) <<") ";
+				if(col == columns - 1){
+					output<<" |\n";
+				}
+			}
+		}
+
+		output<<"\n\nCategories:\n\n";
+		for(json::iterator jt = it.value()["categories"].begin();
+				jt != it.value()["categories"].end();
+				++jt){
+			output<<"+ "<<p(jt.value())<<"\n";
+			if(!jsonListContains(categories, jt.value())){
+				categories.push_back(jt.value());
+			}
+		}
+	}
+
+	return output;
+}
+
+//// List all the categories
+stringstream categoryList(json& categories){
+	stringstream output;
+
+	output <<"\n## Categories\n\n";
+	for(json::iterator it = categories.begin(); it != categories.end(); ++it){
+		output<<"+ "<< p(it.value()) <<"\n";
+	}
+
+	return output;
+}
+
 int main(int argc, char* argv[]){
 	if(argc > 2){
 		cout<<"Invalid command-line arguments\n";
 		exit(1);
 	}
 
-	json db, mods, categories, url;
+	json db, mods, categories;
 	int unsigned jsize;
 	char columns;
 	ifstream ifile, inifile;
@@ -73,6 +186,14 @@ int main(int argc, char* argv[]){
 	}
 
 	INI::Parser config(inifile);
+
+	if(config.top()["game"]==""){
+		cout <<"A game needs to be specified in config.ini\n";
+		exit(1);
+	}else{
+		game = config.top()["game"];
+	}
+
 	if(config.top()["category"]!=""){
 		category = config.top()["category"];
 	}
@@ -85,90 +206,17 @@ int main(int argc, char* argv[]){
 
 	columns = atoi( config.top()["columns"].c_str() );
 
-	url["mod"] = "https://www.nexusmods.com/skyrim/mods/";
-	url["image"] = "https://staticdelivery.nexusmods.com/mods/110/images/";
-
 	ifile >> db;
 	mods = db["Mods"];
 	
 	output	<<"# Skyrim\n\n"
 		<<"## Mods\n\n";
+	
+	output << filterCategory(mods, category, columns).rdbuf();
+	
+	output << modMasterList(mods, categories, columns).rdbuf();
 
-	if(category!="null"){
-		stringstream line1, line2;
-		char col = 0;
-
-		output	<<"### Category: "<<category<<"\n\n";
-		line1	<<"|";
-		line2	<<"|";
-		for(char i = 0; i < columns; i++){
-			line1 <<"   |";
-			line2 <<"---|";
-		}
-		output	<< line1.rdbuf() << "\n" << line2.rdbuf() << "\n";
-
-		line1.str(string());
-		line2.str(string());
-		for(json::iterator it = mods.begin(); it != mods.end(); ++it){
-			if( jsonListContains(it.value()["categories"], category) ){
-				line1<<"| ["<<it.key()<<"](#"<<linkify(it.key())<<") ";
-				line2<<"| ![]("<< p(url["image"]) << p(it.value()["main image"]) <<") ";
-				col++;
-				if(col > columns - 1){
-					col = 0;
-					output	<< line1.rdbuf() << "\n" << line2.rdbuf() << "\n";
-					line1.str(string());
-					line2.str(string());
-				}
-			}
-		}
-		if(col > 0){
-			output	<< line1.rdbuf() << "\n" << line2.rdbuf() << "\n";
-		}
-	}
-
-	output <<"### Mod master list\n\n";
-	for(json::iterator it = mods.begin(); it != mods.end(); ++it){
-		stringstream line1, line2;
-
-		output	<<"\n#### "<<it.key()<<"\n\n"
-
-			<< p(it.value()["description"]) <<"\n\n"
-			<<"[Nexus link](" << p(url["mod"]) << it.value()["id"] << ")\n\n";
-
-		line1	<<"| Images | ![]("<< p(url["image"]) << p(it.value()["main image"]) <<") |";
-		line2	<<"| ------ |:---:|";
-		for(char i = 0; i < (columns - 2); i++){
-			line1 <<"   |";
-			line2 <<"---|";
-		}
-		output	<< line1.rdbuf() << "\n" << line2.rdbuf() << "\n";
-		{	char col = 0;
-			for(json::iterator jt = it.value()["images"].begin();
-					jt != it.value()["images"].end();
-					++jt, col = (col == columns - 1) ? 0 : col + 1 ){
-				output<<"| ![]("<< p(url["image"]) << p(jt.value()) <<") ";
-				if(col == columns - 1){
-					output<<" |\n";
-				}
-			}
-		}
-
-		output<<"\n\nCategories:\n\n";
-		for(json::iterator jt = it.value()["categories"].begin();
-				jt != it.value()["categories"].end();
-				++jt){
-			output<<"+ "<<p(jt.value())<<"\n";
-			if(!jsonListContains(categories, jt.value())){
-				categories.push_back(jt.value());
-			}
-		}
-	}
-
-	output <<"\n### Categories\n\n";
-	for(json::iterator it = categories.begin(); it != categories.end(); ++it){
-		output<<"+ "<< p(it.value()) <<"\n";
-	}
+	output << categoryList(categories).rdbuf();
 
 	ofile << output.rdbuf();
 
